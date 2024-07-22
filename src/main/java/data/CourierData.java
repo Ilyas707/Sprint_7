f2cc14b783431495acfef4977ca1fbf9917c75d9
Ilyas Ganiev <ganiev.ilyas@tatar.ru>
2024-07-22 11:26:00 +0300

    Added @DaTa from the Lombok library

--- src/main/java/data/CourierData.java
+++ src/main/java/data/CourierData.java
@@ -1,5 +1,8 @@
 package data;
 
+import lombok.Data;
+
+@Data
 public class CourierData {
     private String login;
     private String password;
@@ -8,31 +11,6 @@ public class CourierData {
     public CourierData(String login, String password, String firstName) {
         this.login = login;
         this.password = password;
-        this.firstName = firstName;
-    }
-
-
-    public String getLogin() {
-        return login;
-    }
-
-    public void setLogin(String login) {
-        this.login = login;
-    }
-
-    public String getPassword() {
-        return password;
-    }
-
-    public void setPassword(String password) {
-        this.password = password;
-    }
-
-    public String getFirstName() {
-        return firstName;
-    }
-
-    public void setFirstName(String firstName) {
         this.firstName = firstName;
     }
 }
